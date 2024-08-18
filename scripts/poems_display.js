531108eb7518a520bce5b2adf24bd04b6b25b6d4
Abaan5467 <mohammad5467@students.dpssgr.com>
2024-08-18 13:18:07 +0530

    added poetry-specific backgrounds

--- scripts/poems_display.js
+++ scripts/poems_display.js
@@ -1,13 +1,22 @@
 // Retrieve data from localStorage
-const data = localStorage.getItem('poemData');
-const title = localStorage.getItem('poemName');
+const data = localStorage.getItem("poemData");
+const title = localStorage.getItem("poemName");
+
+const bgs = {
+    "The Provider who Provides" : "url('../images/poetry-specific-bgs/The Provider who Provides.jpg')",
+    "A Bankrupt Billionaire" : "url('../images/poetry-specific-bgs/A Bankrupt Billionaire.jpg')",
+    "An Uncommon Textbook" : "url('../images/poetry-specific-bgs/An Uncommon Textbook.jpg')",
+    "A Shining Star": "url('../images/poetry-specific-bgs/A Shining Star.jpg')",
+    "A Calm Carnivore": "url('../images/poetry-specific-bgs/A Calm Carnivore.jpg')",
+    "Re-routed": "url('../images/poetry-specific-bgs/Re-routed.jpg')",
+}
 
 // Check if data exists
 if (data) {
     // Do something with the data
-    console.log(data)
-    document.getElementById('title').innerHTML = title;
-    document.getElementById('main').innerHTML = data;
+    document.getElementById("title").innerHTML = title;
+    document.getElementById("main").innerHTML = data;
+    document.getElementsByClassName("container")[0].style.backgroundImage = bgs[title];
 } else {
-    console.log('No data found in localStorage');
+    console.log("No data found in localStorage");
 }
